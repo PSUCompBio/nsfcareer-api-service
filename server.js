11992fc05021d52d6ba41e499c58d6dcb6a269a6
Raman Kumar <ramana.kumar@brihaspatitech.com>
2020-08-19 09:03:40 +0530

    Config chnages

--- server.js
+++ server.js
@@ -77,35 +77,35 @@ if (cluster.isMaster) {
     // ======================================
     //       CONFIGURING AWS SDK & EXPESS
     // ======================================
-    var config = {
-        "awsAccessKeyId": process.env.AWS_ACCESS_KEY_ID,
-        "awsSecretAccessKey": process.env.AWS_ACCESS_SECRET_KEY,
-        "avatar3dClientId": process.env.AVATAR_3D_CLIENT_ID,
-        "avatar3dclientSecret": process.env.AVATAR_3D_CLIENT_SECRET,
-        "region" : process.env.REGION,
-        "usersbucket": process.env.USERS_BUCKET,
-        "apiVersion" : process.env.API_VERSION,
-        "jwt_secret" : process.env.JWT_SECRET,
-        "email_id" : process.env.EMAIL_ID,
-        "mail_list" : process.env.MAIL_LIST,
-        "ComputeInstanceEndpoint" : process.env.COMPUTE_INSTANCE_ENDPOINT,
-        "userPoolId": process.env.USER_POOL_ID,
-        "ClientId" : process.env.CLIENT_ID,
-        "react_website_url" : process.env.REACT_WEBSITE_URL,
-        "simulation_result_host_url" : process.env.SIMULATION_RESULT_HOST_URL,
-        "jobQueueBeta" : process.env.JOB_QUEUE_BETA,
-        "jobDefinitionBeta" : process.env.JOB_DEFINITION_BETA,
-        "jobQueueProduction" : process.env.JOB_QUEUE_PRODUCTION,
-        "jobDefinitionProduction" : process.env.JOB_DEFINITION_PRODUCTION,
-        "simulation_bucket" : process.env.SIMULATION_BUCKET,
-        "queue_x" : process.env.QUEUE_X,
-        "queue_y" : process.env.QUEUE_Y,
-        "queue_beta" : process.env.QUEUE_BETA
-    };
+    // var config = {
+    //     "awsAccessKeyId": process.env.AWS_ACCESS_KEY_ID,
+    //     "awsSecretAccessKey": process.env.AWS_ACCESS_SECRET_KEY,
+    //     "avatar3dClientId": process.env.AVATAR_3D_CLIENT_ID,
+    //     "avatar3dclientSecret": process.env.AVATAR_3D_CLIENT_SECRET,
+    //     "region" : process.env.REGION,
+    //     "usersbucket": process.env.USERS_BUCKET,
+    //     "apiVersion" : process.env.API_VERSION,
+    //     "jwt_secret" : process.env.JWT_SECRET,
+    //     "email_id" : process.env.EMAIL_ID,
+    //     "mail_list" : process.env.MAIL_LIST,
+    //     "ComputeInstanceEndpoint" : process.env.COMPUTE_INSTANCE_ENDPOINT,
+    //     "userPoolId": process.env.USER_POOL_ID,
+    //     "ClientId" : process.env.CLIENT_ID,
+    //     "react_website_url" : process.env.REACT_WEBSITE_URL,
+    //     "simulation_result_host_url" : process.env.SIMULATION_RESULT_HOST_URL,
+    //     "jobQueueBeta" : process.env.JOB_QUEUE_BETA,
+    //     "jobDefinitionBeta" : process.env.JOB_DEFINITION_BETA,
+    //     "jobQueueProduction" : process.env.JOB_QUEUE_PRODUCTION,
+    //     "jobDefinitionProduction" : process.env.JOB_DEFINITION_PRODUCTION,
+    //     "simulation_bucket" : process.env.SIMULATION_BUCKET,
+    //     "queue_x" : process.env.QUEUE_X,
+    //     "queue_y" : process.env.QUEUE_Y,
+    //     "queue_beta" : process.env.QUEUE_BETA
+    // };
 
     const subject_signature = fs.readFileSync("data/base64")
 
-    // var config = require('./config/configuration_keys.json');
+    var config = require('./config/configuration_keys.json');
     var config_env = config;
 
     //AWS.config.loadFromPath('./config/configuration_keys.json');
@@ -1094,15 +1094,22 @@ if (cluster.isMaster) {
                     getPlayerSimulationFile(acc_data)
                     .then(file_data => {
                         // console.log('image_data',image_data)
-                         imageData = file_data;
+                        imageData = file_data;
+                        
                         if (imageData.ouput_file_path && imageData.ouput_file_path != 'null') {
-                            let file_path = imageData.ouput_file_path;
+                            let file_path = image_data.ouput_file_path;
                             file_path = file_path.replace(/'/g, "");
                             return getFileFromS3(file_path);
+                        } else {
+                            if (imageData.root_path && imageData.root_path != 'null') {
+                                let summary_path = imageData.root_path + imageData.image_id + '_ouput.json';
+                                return getFileFromS3(summary_path);
+                            }
                         }
                     }).then(output_file => {
                         outputFile = output_file;
-                        outputFile = JSON.parse(outputFile.Body.toString('utf-8'));
+                        if (output_file)
+                            outputFile = JSON.parse(outputFile.Body.toString('utf-8'));
                         console.log('outputFile',outputFile)
                         // if (imageData.path && imageData.path != 'null')
                         //     return getFileFromS3(imageData.path);
@@ -1112,12 +1119,13 @@ if (cluster.isMaster) {
                                 'JsonFile':outputFile
                             }
                         })
-                    }).catch(err => {
-                        res.send({
-                            message: "failure",
-                            error: err
-                        })
                     })
+                    // .catch(err => {
+                    //     res.send({
+                    //         message: "failure",
+                    //         error: err
+                    //     })
+                    // })
                 })
 
                 
@@ -1726,7 +1734,7 @@ if (cluster.isMaster) {
     })
 
     // Configuring port for APP
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 5000;
     const server = app.listen(port, function () {
         console.log('Magic happens on ' + port);
     });
